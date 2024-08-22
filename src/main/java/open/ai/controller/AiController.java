c719127bfdcdf50bdd0ed632609971a17b51f7dd
Vitor Tinelli <vitor.tinelli@thomsonreuters.com>
2024-08-22 10:37:57 -0300

    1.3.7 | feat: CORS all alowed Origins

--- src/main/java/open/ai/controller/AiController.java
+++ src/main/java/open/ai/controller/AiController.java
@@ -2,15 +2,16 @@ package open.ai.controller;
 
 import java.util.List;
 import java.util.Set;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import open.ai.requests.ConversationDataRequestBody;
 import open.ai.responses.AiResponse;
 import open.ai.service.AiService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ai")
@@ -19,7 +20,7 @@ public class AiController {
 
   private final AiService aiService;
 
-  @GetMapping
+  @PostMapping
   public ResponseEntity<String> call(@RequestBody ConversationDataRequestBody conversation) {
     return ResponseEntity.ok(aiService.call(conversation.getId(), conversation.getMessage()));
   }
@@ -31,7 +32,7 @@ public class AiController {
         aiService.listOutputParser(conversation.getId(), conversation.getMessage()));
   }
 
-  @GetMapping("/prompt")
+  @PostMapping("/prompt")
   public ResponseEntity<AiResponse> prompt(@RequestBody ConversationDataRequestBody conversation) {
     return ResponseEntity.ok((
         aiService.promptCall(conversation.getId(), conversation.getMessage())));
